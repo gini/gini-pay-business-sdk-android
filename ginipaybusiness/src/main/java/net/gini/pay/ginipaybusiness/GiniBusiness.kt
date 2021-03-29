3bf57accc3ca92f1b75f96fa9e39094558e7d981
Alexandru Gabor <alexandru.gabor@halcyonmobile.com>
2021-03-29 18:34:26 +0300

    Add support for setting document for review with document id and optional payment details

--- ginipaybusiness/src/main/java/net/gini/pay/ginipaybusiness/GiniBusiness.kt
+++ ginipaybusiness/src/main/java/net/gini/pay/ginipaybusiness/GiniBusiness.kt
@@ -2,26 +2,88 @@ package net.gini.pay.ginipaybusiness
 
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import net.gini.android.DocumentManager
+import net.gini.android.Gini
 import net.gini.android.models.Document
-import net.gini.pay.ginipaybusiness.review.model.ExtractionResult
-import net.gini.pay.ginipaybusiness.review.model.getExtractions
+import net.gini.pay.ginipaybusiness.review.model.PaymentDetails
+import net.gini.pay.ginipaybusiness.review.model.ResultWrapper
+import net.gini.pay.ginipaybusiness.review.model.toPaymentDetails
+import net.gini.pay.ginipaybusiness.review.model.wrapToResult
 
+/**
+ * TODO
+ */
 class GiniBusiness(
-    private val documentManager: DocumentManager
+    giniApi: Gini
 ) {
+    private val documentManager = giniApi.documentManager
 
-    private val _documentFlow = MutableStateFlow<ExtractionResult>(ExtractionResult.Loading)
-    val documentFlow: StateFlow<ExtractionResult> = _documentFlow
+    private var documentId: String? = null
 
+    private val _documentFlow = MutableStateFlow<ResultWrapper<Document>>(ResultWrapper.Loading())
+
+    /**
+     * A flow for getting the [Document] set for review [setDocumentForReview].
+     *
+     * It always starts with [ResultWrapper.Loading] when setting a document.
+     * [Document] will be wrapped in [ResultWrapper.Success], otherwise the throwable will
+     * be in a [ResultWrapper.Error].
+     *
+     * It never completes.
+     */
+    val documentFlow: StateFlow<ResultWrapper<Document>> = _documentFlow
+
+    private val _paymentFlow = MutableStateFlow<ResultWrapper<PaymentDetails>>(ResultWrapper.Loading())
+
+    /**
+     * A flow for getting extracted [PaymentDetails] for the document set for review (see [setDocumentForReview]).
+     *
+     * It always starts with [ResultWrapper.Loading] when setting a document.
+     * [PaymentDetails] will be wrapped in [ResultWrapper.Success], otherwise the throwable will
+     * be in a [ResultWrapper.Error].
+     *
+     * It never completes.
+     */
+    val paymentFlow: StateFlow<ResultWrapper<PaymentDetails>> = _paymentFlow
+
+    /**
+     * Sets a [Document] for review. Results can be collected from [documentFlow] and [paymentFlow].
+     *
+     * @param document document received from Gini API.
+     */
     suspend fun setDocumentForReview(document: Document) {
-        _documentFlow.value = ExtractionResult.Loading
-        try {
-            val extractions = documentManager.getExtractions(document)
-            // TODO get image url for pages and include it in result
-            _documentFlow.value = ExtractionResult.Success(getExtractions(document, emptyList(), extractions))
-        } catch (throwable: Throwable) {
-            _documentFlow.value = ExtractionResult.Error(throwable)
+        documentId = document.id
+        _documentFlow.value = ResultWrapper.Success(document)
+        _paymentFlow.value = ResultWrapper.Loading()
+
+        _paymentFlow.value = wrapToResult {
+            documentManager.getExtractions(document).toPaymentDetails()
+        }
+    }
+
+    /**
+     * Sets a [Document] for review. Results can be collected from [documentFlow] and [paymentFlow].
+     *
+     * @param documentId id of the document returned by Gini API.
+     * @param paymentDetails optional [PaymentDetails] for the document corresponding to [documentId]
+     */
+    suspend fun setDocumentForReview(documentId: String, paymentDetails: PaymentDetails? = null) {
+        this.documentId = documentId
+        _paymentFlow.value = ResultWrapper.Loading()
+        _documentFlow.value = ResultWrapper.Loading()
+        _documentFlow.value = wrapToResult {
+            documentManager.getDocument(documentId)
+        }
+        if (paymentDetails != null) {
+            _paymentFlow.value = ResultWrapper.Success(paymentDetails)
+        } else {
+            when (val documentResult = documentFlow.value) {
+                is ResultWrapper.Success -> {
+                    _paymentFlow.value = wrapToResult { documentManager.getExtractions(documentResult.value).toPaymentDetails() }
+                }
+                is ResultWrapper.Error -> {
+                    _paymentFlow.value = ResultWrapper.Error(Throwable("Failed to get document"))
+                }
+            }
         }
     }
 }
--- ginipaybusiness/src/main/java/net/gini/pay/ginipaybusiness/review/model/ExtractionResult.kt
+++ ginipaybusiness/src/main/java/net/gini/pay/ginipaybusiness/review/model/ExtractionResult.kt
@@ -1,7 +1,13 @@
 package net.gini.pay.ginipaybusiness.review.model
 
-sealed class ExtractionResult {
-    class Success(val extractions: Extractions) : ExtractionResult()
-    class Error(val error: Throwable) : ExtractionResult()
-    object Loading : ExtractionResult()
+sealed class ResultWrapper<out T> {
+    class Success<T>(val value: T) : ResultWrapper<T>()
+    class Error<T>(val error: Throwable) : ResultWrapper<T>()
+    class Loading<T> : ResultWrapper<T>()
+}
+
+suspend inline fun <T> wrapToResult(crossinline block: suspend () -> T): ResultWrapper<T> = try {
+    ResultWrapper.Success(block())
+} catch (throwable: Throwable) {
+    ResultWrapper.Error(throwable)
 }
