d5cabb5b1e1f4817e91f398d24335643799cfbcc
Alpar Szotyori <alpar@gini.net>
2021-06-02 14:39:44 +0200

    Update the example app to show the close button

--- app/src/main/java/net/gini/pay/app/review/ReviewActivity.kt
+++ app/src/main/java/net/gini/pay/app/review/ReviewActivity.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
+import androidx.core.view.isGone
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
 import androidx.fragment.app.commit
@@ -14,20 +15,46 @@ import dev.chrisbanes.insetter.applyInsetter
 import net.gini.pay.app.R
 import net.gini.pay.app.databinding.ActivityReviewBinding
 import net.gini.pay.ginipaybusiness.GiniBusiness
+import net.gini.pay.ginipaybusiness.review.ReviewConfiguration
 import net.gini.pay.ginipaybusiness.review.ReviewFragment
+import net.gini.pay.ginipaybusiness.review.ReviewFragmentListener
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ReviewActivity : AppCompatActivity() {
 
     private val viewModel: ReviewViewModel by viewModel()
 
+    private val reviewFragmentListener = object: ReviewFragmentListener {
+        override fun onCloseReview() {
+            finish()
+        }
+    }
+
+    /**
+     * Set it to `true` to show the close button instead of the toolbar.
+     *
+     * When 'false' then also set the dimen resource `gpb_page_padding_top` to equal
+     * `toolbar_height` to prevent the toolbar overlapping the top of the page.
+     */
+    private val showCloseButton = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         viewModel.giniBusiness.setSavedStateRegistryOwner(this, viewModel.viewModelScope)
+
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        supportFragmentManager.fragmentFactory = ReviewFragmentFactory(viewModel.giniBusiness)
+
+        supportFragmentManager.fragmentFactory = ReviewFragmentFactory(
+            viewModel.giniBusiness,
+            ReviewConfiguration(showCloseButton = showCloseButton),
+            reviewFragmentListener
+        )
+
         super.onCreate(savedInstanceState)
+
         val binding = ActivityReviewBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.toolbar.isGone = showCloseButton
 
         binding.toolbar.applyInsetter {
             type(statusBars = true) {
@@ -56,8 +83,11 @@ class ReviewActivity : AppCompatActivity() {
 }
 
 
-class ReviewFragmentFactory(private val giniBusiness: GiniBusiness) : FragmentFactory() {
+class ReviewFragmentFactory(private val giniBusiness: GiniBusiness,
+                            private val configuration: ReviewConfiguration,
+                            private val listener: ReviewFragmentListener
+) : FragmentFactory() {
     override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
-        return ReviewFragment(giniBusiness)
+        return ReviewFragment(giniBusiness, configuration, listener)
     }
 }
